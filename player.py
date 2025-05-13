18d06e81fe9fe4b02b19400386218d14dca03dab
Barsha2509 <77188609+Barsha2509@users.noreply.github.com>
2025-05-13 14:17:21 -0400

    Create player.py

---
+++ player.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.setheading(90)
+        self.goto(0,-280)
+
+    def up(self):
+        self.forward(10)
+
+    def start_over(self):
+        self.goto(0,-280)
+
